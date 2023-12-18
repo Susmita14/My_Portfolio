65bf6217f965e3b803979f549f1b3573ad954df2
susmita <119124175+Susmita14@users.noreply.github.com>
2023-12-19 01:07:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-menu");
+    menu.classList.toggleMenu("open");
+    icon.classList.toggleMenu("open");
+    
+}
